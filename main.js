8858da683aee0907304b899a5132527e31068fdb
vincenzovolpe <vinci792010@gmail.com>
2019-11-25 18:26:32 +0100

    Aggiunti alcuni commenti e chiusura dell'humburger menu in caso rimane aperto sul resize della pagina

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+// Mostra il menù hamburger quando clicco sull'icona dell'hamburger
 $(".header-right i").click(function() {
     $('.hamburger-menu').show();
 });
-
+// Nascondo il menù hamburger quando clicco sulla X delmenù humburger
 $('.hamburger-menu i').click(function() {
     $('.hamburger-menu').hide();
 })
+
+// Faccio in modo che facendo resize del browser e il menù mobile è ancora aperto ritorna automaticamente il menù desktop
+$(window).resize(function(){
+
+       if ($(window).width() > 1000) {
+
+            $('.hamburger-menu').hide();
+
+       }
+
+});
